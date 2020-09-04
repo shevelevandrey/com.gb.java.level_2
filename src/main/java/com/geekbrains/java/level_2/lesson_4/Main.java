ae4e97721d836c0e54b44892ec197dc511eb8b43
shevelevandrey <shevelevandrey@mail.ru>
2020-09-04 16:12:20 +0300

    Doing lesson 4.

---
+++ src/main/java/com/geekbrains/java/level_2/lesson_4/Main.java
@@ -0,0 +1,9 @@
+package com.geekbrains.java.level_2.lesson_4;
+
+import com.geekbrains.java.level_2.lesson_4.Chat.Chat;
+
+public class Main {
+    public static void main(String[] args) {
+        new Chat();
+    }
+}
